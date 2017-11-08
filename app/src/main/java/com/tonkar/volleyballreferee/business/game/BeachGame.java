4a5986870e713f08a3d3d68dbdb534c214b7cb27
GuillaumeVT <31916395+guillaumevt@users.noreply.github.com>
2017-11-08 14:23:15 +0000

    The position of the second beach player is automatically set

--- app/src/main/java/com/tonkar/volleyballreferee/business/game/BeachGame.java
+++ app/src/main/java/com/tonkar/volleyballreferee/business/game/BeachGame.java
@@ -1,6 +1,7 @@
 package com.tonkar.volleyballreferee.business.game;
 
 import com.tonkar.volleyballreferee.business.team.Team;
+import com.tonkar.volleyballreferee.interfaces.PositionType;
 import com.tonkar.volleyballreferee.rules.Rules;
 import com.tonkar.volleyballreferee.business.team.BeachTeam;
 import com.tonkar.volleyballreferee.interfaces.ActionOriginType;
@@ -49,6 +50,15 @@ public class BeachGame extends Game {
         if (getRules().isChangeSidesEvery7Points() && combinedScores > 0 && (combinedScores % period) == (period - 1)) {
             swapTeams(ActionOriginType.APPLICATION);
         }
+    }
+
+    @Override
+    public void substitutePlayer(TeamType teamType, int number, PositionType positionType) {
+        super.substitutePlayer(teamType, number, positionType);
+
+        int otherNumber = (number == 1) ? 2 : 1;
+        PositionType otherPositionType = (PositionType.POSITION_1.equals(positionType) ? PositionType.POSITION_2 : PositionType.POSITION_1);
+        super.substitutePlayer(teamType, otherNumber, otherPositionType);
     }
 
 }
