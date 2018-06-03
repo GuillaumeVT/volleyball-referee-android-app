3d157eb67335a0cbc5d5be4295d02c367168d121
GuillaumeVT <31916395+guillaumevt@users.noreply.github.com>
2018-06-03 23:03:16 +0100

    Added the translations for the notifications parameter

--- app/src/main/java/com/tonkar/volleyballreferee/business/data/WebUtils.java
+++ app/src/main/java/com/tonkar/volleyballreferee/business/data/WebUtils.java
@@ -8,6 +8,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.Volley;
+import com.tonkar.volleyballreferee.BuildConfig;
 import com.tonkar.volleyballreferee.interfaces.data.AsyncGameRequestListener;
 
 import java.net.HttpURLConnection;
@@ -18,19 +19,20 @@ import java.util.Map;
 
 public class WebUtils {
 
-    public static String BASE_URL                 = "https://www.volleyball-referee.com";
-    public static String SEARCH_URL               = BASE_URL + "/search";
-    public static String LIVE_URL                 = BASE_URL + "/search/live";
-    public static String VIEW_URL                 = BASE_URL + "/game/%d";
-    public static String USER_URL                 = BASE_URL + "/user";
-    public static String GAME_API_URL             = BASE_URL + "/api/manage/game/%d";
-    public static String SET_API_URL              = GAME_API_URL + "/set/%d";
-    public static String HAS_MESSAGE_URL          = BASE_URL + "/api/message/has";
-    public static String MESSAGE_URL              = BASE_URL + "/api/message";
-    public static String GAME_CODE_URL            = BASE_URL + "/api/view/game/code/%d";
-    public static String USER_GAME_URL            = BASE_URL + "/api/user/game";
-    public static String USER_SCHEDULED_GAMES_URL = USER_GAME_URL + "/available";
-
+    private static String BASE_URL                 = BuildConfig.SERVER_ADDRESS;
+    public static  String SEARCH_URL               = BASE_URL + "/search";
+    public static  String LIVE_URL                 = BASE_URL + "/search/live";
+    public static  String VIEW_URL                 = BASE_URL + "/game/%d";
+    public static  String USER_URL                 = BASE_URL + "/user";
+    public static  String GAME_API_URL             = BASE_URL + "/api/manage/game/%d";
+    public static  String SET_API_URL              = GAME_API_URL + "/set/%d";
+    public static  String HAS_MESSAGE_URL          = BASE_URL + "/api/message/has";
+    public static  String MESSAGE_URL              = BASE_URL + "/api/message";
+    private static String GAME_CODE_URL            = BASE_URL + "/api/view/game/code/%d";
+    private static String USER_GAME_URL            = BASE_URL + "/api/user/game";
+    private static String USER_SCHEDULED_GAMES_URL = USER_GAME_URL + "/available";
+    private static String USER_RULES_URL           = BASE_URL + "/api/user/rules";
+    private static String USER_TEAM_URL            = BASE_URL + "/api/user/team";
 
     private static WebUtils     sWebUtils;
     private        RequestQueue mRequestQueue;
