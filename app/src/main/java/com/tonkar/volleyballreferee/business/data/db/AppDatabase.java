b4ffc0222f07fcf3c6a2f58478432a9176f6d619
GuillaumeVT <31916395+guillaumevt@users.noreply.github.com>
2018-10-07 14:37:23 +0100

    Improved data sync

--- app/src/main/java/com/tonkar/volleyballreferee/business/data/db/AppDatabase.java
+++ app/src/main/java/com/tonkar/volleyballreferee/business/data/db/AppDatabase.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = { RulesEntity.class, TeamEntity.class, GameEntity.class, FullGameEntity.class, SyncEntity.class }, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
@@ -13,7 +15,7 @@ public abstract class AppDatabase extends RoomDatabase {
 
     public static AppDatabase getInstance(Context context) {
         if (sInstance == null) {
-            sInstance = Room.databaseBuilder(context, AppDatabase.class, "vbr-db").allowMainThreadQueries().build();
+            sInstance = Room.databaseBuilder(context, AppDatabase.class, "vbr-db").addMigrations(MIGRATION_1_2).allowMainThreadQueries().build();
         }
 
         return sInstance;
@@ -28,5 +30,12 @@ public abstract class AppDatabase extends RoomDatabase {
     public abstract FullGameDao fullGameDao();
 
     public abstract SyncDao syncDao();
+
+    private static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            database.execSQL("CREATE TABLE `syncs` (`item` TEXT NOT NULL, `type` TEXT NOT NULL, PRIMARY KEY(`item`))");
+        }
+    };
 
 }
